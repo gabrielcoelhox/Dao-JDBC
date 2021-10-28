3fc877dd3a6f2e248610a37d8ab9a57ed11619dd
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-10-28 17:18:09 -0300

    Implementando findAll

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -84,8 +84,46 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	@Override
+	// Buscar todos os vendedores
 	public List<Seller> findAll() {
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+
+			rs = st.executeQuery();
+			
+			// Testar se foi obtido algum resultado.
+			
+			List<Seller> list = new ArrayList<Seller>();
+			Map<Integer, Department> map = new HashMap<>(); // Controle para evitar repetição de departamento
+				
+			while (rs.next()) {
+				
+				// Testar se o departamento já existe
+				// Buscar no map um departamento com determinado Id, se não existir irá retornar null
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				// Se for nulo, instanciar o departamento
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;	
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
