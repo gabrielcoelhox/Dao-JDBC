a8e54a873432982e5539ed2ce81364a356fbd944
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-10-29 15:55:00 -0300

    Implementando insert para departamentos

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -11,7 +11,7 @@ import model.entities.Seller;
 public class Program2 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
@@ -29,7 +29,14 @@ public class Program2 {
 		for (Department obj : list) {
 			System.out.println(obj);
 		}
-		
+
+		// Inserir um departamento
+		System.out.println("\n=== TEST 3: department Insert ==="); 
+		Department newDepartment = new Department(null, "Food");
+		departmentDao.insert(newDepartment);
+	
+		System.out.println("Inserted! New id = " + newDepartment.getId());
+
 		sc.close();
 	}
 }
